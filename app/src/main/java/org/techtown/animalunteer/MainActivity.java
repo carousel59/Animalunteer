997f916dbb9c6169ad2b911948dfcca3e7dbf54b
carousel59 <carousel59@naver.com>
2020-12-19 22:50:09 +0900

    add map

--- app/src/main/java/org/techtown/animalunteer/MainActivity.java
+++ app/src/main/java/org/techtown/animalunteer/MainActivity.java
@@ -2,7 +2,15 @@ package org.techtown.animalunteer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
