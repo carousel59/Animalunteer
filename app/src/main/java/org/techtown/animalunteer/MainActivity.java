10239b02df5a2acf33c273e568ed10c25f639262
carousel59 <carousel59@naver.com>
2020-12-20 02:06:46 +0900

    add map fragment

--- app/src/main/java/org/techtown/animalunteer/MainActivity.java
+++ app/src/main/java/org/techtown/animalunteer/MainActivity.java
@@ -3,6 +3,7 @@ package org.techtown.animalunteer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 
--- app/src/main/java/org/techtown/animalunteer/MapFragment.java
+++ app/src/main/java/org/techtown/animalunteer/MapFragment.java
@@ -1,10 +1,15 @@
 package org.techtown.animalunteer;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,6 +42,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mapView = (MapView)rootView.findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+
         return rootView;
     }
 
@@ -62,8 +68,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         MapsInitializer.initialize(this.getActivity());
 
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(35.12, 126.92), 14);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(35.84686, 127.12928), 14);
         googleMap.animateCamera(cameraUpdate);
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.14, 126.92)).title("루프리코리아"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.84686, 127.12928)).title("전북대"));
     }
+
 }
