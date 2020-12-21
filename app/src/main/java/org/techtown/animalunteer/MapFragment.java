fa50aa8624e3757b8c075894642921eb31f69691
dayoung <ekffkddl0529@naver.com>
2020-12-21 15:51:22 +0900

    add mark linkify

--- app/src/main/java/org/techtown/animalunteer/MapFragment.java
+++ app/src/main/java/org/techtown/animalunteer/MapFragment.java
@@ -1,5 +1,6 @@
 package org.techtown.animalunteer;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -13,6 +14,9 @@ import androidx.fragment.app.Fragment;
 
 import android.provider.Settings;
 import android.text.Layout;
+import android.text.Spannable;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -51,7 +55,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mapView = (MapView)rootView.findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
-
 
         return rootView;
     }
@@ -92,16 +95,43 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 .snippet("010-3659-2380\n" +
                         "전라북도 익산시 서동로42길 77 (용제동) 16통 1반\n" +
                         "https://band.us/@dogshelter"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.960714650804206, 126.81577296139993)).title("군산 유기동물보호센터"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(34.85372948216843, 126.43027889760849)).title("목포시 유기동물보호소"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(34.74855690548188, 126.4859461571284)).title("삼호 유기견 보호소"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(36.02332725365387, 126.98921944437555)).title("하얀강아지"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.15567471475577, 126.8354954424672)).title("광주동물보호협회 위드")); //광주
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.008368335584976, 127.03245337255996)).title("화순동물보호협회 위드")); //화순
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.80363099809347, 126.88053702599807)).title("동행세상"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.01600720920167, 126.71082508179154)).title("나주 천사의 집"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.0160752774857, 127.37918695480633)).title("순천 유기동물 보호소"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(34.761940865406906, 127.66178693838108)).title("여수 여미지 보호소"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.960714650804206, 126.81577296139993)).title("군산 유기동물보호센터")
+                .snippet("010-3675-9330\n" +
+                        "전라북도 군산시 대야면 보덕안정길 108-20 (대야면) 군산도그랜드\n" +
+                        "https://www.facebook.com/groups/2291246801097054/\n" +
+                        "instagram@gunsan_dogcat"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(34.85372948216843, 126.43027889760849)).title("목포시 유기동물보호소")
+                .snippet("061-274-3570\n" +
+                        "010-9678-9966\n" +
+                        "전라남도 무안군 삼향읍 석매길 21-5 (삼향읍)"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(34.74855690548188, 126.4859461571284)).title("유기동물 삼호 보호소")
+                .snippet("010-9599-5400\n" +
+                        "전라남도 영암군 삼호읍 녹색로 1117-49 (삼호읍) 영암유기동물 삼호보호소"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(36.02332725365387, 126.98921944437555)).title("하얀강아지")
+                .snippet("010-5247-0440\n" +
+                        "cafe.naver.com/kkkk4428"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.15567471475577, 126.8354954424672)).title("광주동물보호협회 위드")
+                .snippet("062-531-6341\n" +
+                        "cafe.naver.com/lovedog1253")); //광주
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.008368335584976, 127.03245337255996)).title("화순동물보호협회 위드")
+                .snippet("010-4345-5580\n" +
+                        "전라남도 화순군 한천면 덕음로 115 (한천면)\n" +
+                        "cafe.naver.com/lovedog1253")); //화순
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.80363099809347, 126.88053702599807)).title("동행세상")
+                .snippet("instagram@lcfkd\n" +
+                        "https://m.cafe.daum.net/LineageStory/_rec"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.01600720920167, 126.71082508179154)).title("나주 천사의 집")
+                .snippet("010-8620-2222\n" +
+                        "https://cafe.naver.com/angelshousa/30923\n" +
+                        "instagram@naju_1004s\n" +
+                        "facebook@najuangelshouse"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(35.0160752774857, 127.37918695480633)).title("순천 유기동물 보호소")
+                .snippet("061-749-8793\n" +
+                        "전라남도 순천시 승주읍 승주로 628 (승주읍, 농업기술센터)\n" +
+                        "https://cafe.naver.com/dogsfamliy (여기서 봉사신청, 후원)"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(34.761940865406906, 127.66178693838108)).title("여수 여미지 보호소")
+                .snippet("instagram@yeomiji_shelter\n" +
+                        "https://cafe.naver.com/943400"));
 
         googleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
 
@@ -114,6 +144,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             public View getInfoContents(Marker marker) {
 
                 Context context = getActivity();
+
+
 
                 LinearLayout info = new LinearLayout(context);
                 info.setOrientation(LinearLayout.VERTICAL);
@@ -131,9 +163,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 info.addView(title);
                 info.addView(snippet);
 
+                Linkify.addLinks(snippet, Linkify.ALL);
+                snippet.setMovementMethod(LinkMovementMethod.getInstance());
+
                 return info;
             }
         });
+
+
 
     }
 
